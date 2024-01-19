80a6dfc497ee5a5897c3b5b1462279679d1a5fc1
HVCsano <hvcsano@proton.me>
2024-01-19 14:16:34 +0100

    már csak docker kell

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,32 @@
-console.log("Hello via Bun!");
+import express from "express";
+import path from "node:path";
+import bodyParser from "body-parser";
+
+import * as sckk from "./routes/sckk";
+
+const port = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.json());
+
+app.use(bodyParser.json({ limit: "10mb" }));
+
+app.use(express.static("public"));
+
+app.use("/sckk", sckk.router);
+
+app.get("/", (req, res) => {
+	res.sendFile(path.resolve("src/routes/index.html"));
+});
+
+app.use((req, res, next) => {
+	res.status(404);
+	if (req.accepts("html")) {
+		res.sendFile(path.resolve("src/routes/404.html"));
+	}
+});
+
+app.listen(port, () => {
+	console.log(`http://localhost:${port}`);
+});
