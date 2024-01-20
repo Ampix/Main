d3f2bc7b30e8f12e69fe1285c4412b74da5d6cb6
Blintx <balint.retvari@gmail.com>
2024-01-20 21:00:47 +0100

    dizájn

--- src/client/matek/script.ts
+++ src/client/matek/script.ts
@@ -18,33 +18,35 @@ function prime_numbers(num:number):number{
 function check_if_one(num:number):boolean{
     return num!==1;
 }
-function check_if_divisible(num:number,diviser:number):boolean{
-    return num%diviser===0;
-}
 
 function prime_number_(input:number):void{
     let num = input
     let prime_numbers_under_num = [];
     let output = []
     const partial_result =[]
-    for (let index = 2; index < num+1; index++) {
+    for (let index = 2; index < (num/2)+1; index++) {
         prime_numbers_under_num.push(prime_numbers(index));
     }
+    prime_numbers_under_num.push(prime_numbers(num));
     prime_numbers_under_num = prime_numbers_under_num.filter(check_if_one);
     for (let index = 0; index < prime_numbers_under_num.length; index++) {
         const element = prime_numbers_under_num[index];
         
-        while (check_if_divisible(num,element)){
-            output.push(element)
-            partial_result.push(num)
-            num /= element
+        while (num%element===0) {
+            output.push(element);
+            partial_result.push(num);
+            num /= element;
+            console.log("step")
         }
+        
     }
     partial_result.push(1)
     output = output.filter(check_if_one);
     const bal_oldal = document.getElementById("bal_oldal");
     const jobb_oldal = document.getElementById("jobb_oldal");
-    if (bal_oldal!= null && jobb_oldal!= null){
+    if (bal_oldal!= null && jobb_oldal!= null ){
+        bal_oldal.innerHTML = "";
+        jobb_oldal.innerHTML = "";
     for (let i = 0; i < partial_result.length; i++) {
         const element = document.createElement("h2");
         element.innerText = partial_result[i].toString();
