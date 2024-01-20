04c1a78672d55db7c2e62c1dbb8ca9f7fda38a17
Blintx <balint.retvari@gmail.com>
2024-01-20 19:32:52 +0100

    bug fixes

--- src/client/matek/script.ts
+++ src/client/matek/script.ts
@@ -18,17 +18,24 @@ function prime_numbers(num:number):number{
 function check_if_one(num:number):boolean{
     return num!=1;
 }
+function check_if_divisible(num:number,diviser:number):boolean{
+    return num%diviser==0;
+}
 
 function prime_number_(num:number):void{
     let prime_numbers_under_num = [];
     let output = []
+    let partial_result =[]
     for (let index = 2; index < num; index++) {
         prime_numbers_under_num.push(prime_numbers(index));
     }
+    prime_numbers_under_num = prime_numbers_under_num.filter(check_if_one);
     for (let index = 0; index < prime_numbers_under_num.length; index++) {
         const element = prime_numbers_under_num[index];
-        if (num%element ==0){
+        
+        while (check_if_divisible(num,element)){
             output.push(element)
+            partial_result.push(num)
             num /= element
         }
     }
@@ -37,13 +44,16 @@ function prime_number_(num:number):void{
     if (element) {
     element.innerHTML = output.toString();
     }
+
 }
+
 
 
 document.getElementById("prime_number_form")?.addEventListener("submit",(e) =>{
    e.preventDefault();
    const data = new FormData(e.target as HTMLFormElement);
    prime_number_(Object.fromEntries(data.entries()).prime)
+
 })
 
 
