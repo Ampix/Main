cbef14430c39851d2ecb43187534376c9161cae6
Blintx <balint.retvari@gmail.com>
2024-01-20 20:07:03 +0100

    formátolás

--- src/client/matek/script.ts
+++ src/client/matek/script.ts
@@ -1,13 +1,13 @@
 function prime_numbers(num:number):number{
     
-    let counter:number = 0;
+    let counter = 0;
     for (let index = 2; index <= Math.sqrt(num) ; index++) {
-        if (num % index ==0 ){
+        if (num % index ===0 ){
             counter++;
         }
 
     }
-    if (counter == 0){
+    if (counter === 0){
         return num;
     }
     
@@ -16,17 +16,18 @@ function prime_numbers(num:number):number{
 }
 
 function check_if_one(num:number):boolean{
-    return num!=1;
+    return num!==1;
 }
 function check_if_divisible(num:number,diviser:number):boolean{
-    return num%diviser==0;
+    return num%diviser===0;
 }
 
-function prime_number_(num:number):void{
+function prime_number_(input:number):void{
+    let num = input
     let prime_numbers_under_num = [];
     let output = []
-    let partial_result =[]
-    for (let index = 2; index < num; index++) {
+    const partial_result =[]
+    for (let index = 2; index < num+1; index++) {
         prime_numbers_under_num.push(prime_numbers(index));
     }
     prime_numbers_under_num = prime_numbers_under_num.filter(check_if_one);
@@ -39,12 +40,24 @@ function prime_number_(num:number):void{
             num /= element
         }
     }
+    partial_result.push(1)
     output = output.filter(check_if_one);
-    const element = document.getElementById("output")
-    if (element) {
-    element.innerHTML = output.toString();
+    const bal_oldal = document.getElementById("bal_oldal");
+    const jobb_oldal = document.getElementById("jobb_oldal");
+    if (bal_oldal!= null && jobb_oldal!= null){
+    for (let i = 0; i < partial_result.length; i++) {
+        const element = document.createElement("h2");
+        element.innerText = partial_result[i].toString();
+        bal_oldal.appendChild(element);
+        
     }
-
+    for (let i = 0; i < output.length; i++) {
+        const element = document.createElement("h2");
+        element.innerText = output[i].toString();
+        jobb_oldal.appendChild(element);
+        
+    }
+    }
 }
 
 
