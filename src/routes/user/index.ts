c43cb50b96e91f73b0c26fd221eb65a09656d347
HVCsano <hvcsano@proton.me>
2024-01-20 18:35:24 +0100

    fix

--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -1,23 +1,23 @@
 import express from "express";
-import path from "node:path"
+import path from "node:path";
 import cookieParser from "cookie-parser";
 
 export const router = express.Router();
 
-router.use(cookieParser(process.env.COOKIE_SECRET))
+router.use(cookieParser(process.env.COOKIE_SECRET));
 
 router.get("/", (req, res) => {
-	if(req.signedCookies["userAuthCode"]){
-		res.send("csa")
-	}else{
-		res.send("nem csa")
+	if (req.signedCookies.userAuthCode) {
+		res.send("csa");
+	} else {
+		res.send("nem csa");
 	}
 });
 
-router.get("/setcookie/:cookie/:forthis", (req,res) => {
-	res.cookie(req.params.cookie,req.params.forthis,{
+router.get("/setcookie/:cookie/:forthis", (req, res) => {
+	res.cookie(req.params.cookie, req.params.forthis, {
 		maxAge: 2000,
-		signed:true
-	})
-	res.redirect("/user")
+		signed: true,
+	});
+	res.redirect("/user");
 });
