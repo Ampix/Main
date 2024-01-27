5054c9c61d5023716deca891d2bbbd02c1289ca4
Blintx <balint.retvari@gmail.com>
2024-01-27 20:17:01 +0100

    ellenőrzés

--- src/client/nemet/script.ts
+++ src/client/nemet/script.ts
@@ -1,13 +1,13 @@
+import { Console } from "node:console";
 import json from "./szavak.json";
 const szavak = json.szavak;
 // console.log(json);
-console.log(szavak[0].magyar === "elkezdeni");
-console.log("pityu");
+
 function szav_valaszt() {
 	const szó = document.getElementById("magyar");
 	if (szó) {
 		const index = 0;
-		console.log(szavak[index].magyar);
+
 		szó.innerText = szavak[index].magyar;
 	}
 }
@@ -18,18 +18,32 @@ function ellenorzo(
 	perfekt: string,
 	prateritum: string,
 ) {
-	console.log("siker");
-	const megoldasindex = szavak.find((element) => element.magyar === magyar);
-	console.log(megoldasindex);
+	const eredmeny = document.getElementById("eredmeny");
+	const valasz = szavak.find((element) => element.magyar === magyar);
+	if (valasz && eredmeny) {
+		const megoldasindex = szavak.indexOf(valasz);
 
-	// if (szavak[megoldasindex].magyar === magyar) {
-	// 	console.log("pryma");
-	// }
+		// console.log(szavak[megoldasindex].magyar === magyar);
+		// console.log(szavak[megoldasindex].perfect === perfekt);
+		// console.log(szavak[megoldasindex].német === nemet);
+		// console.log(szavak[megoldasindex].präteritum === prateritum);
+		if (
+			szavak[megoldasindex].magyar === magyar &&
+			szavak[megoldasindex].német === nemet &&
+			szavak[megoldasindex].präteritum === prateritum &&
+			szavak[megoldasindex].perfect === perfekt
+		) {
+			eredmeny.innerText = "A szavak helyesek";
+		} else {
+			eredmeny.innerText = `A szavak nem helyesek, mert a helyes megoldások a következők ${szavak[megoldasindex].német}, ${szavak[megoldasindex].perfect} és ${szavak[megoldasindex].präteritum}`;
+		}
+	}
 }
 document.getElementById("nemet")?.addEventListener("submit", (e) => {
 	e.preventDefault();
 	const data = new FormData(e.target as HTMLFormElement);
 	const nemet = String(Object.fromEntries(data.entries()).nemet);
+
 	const perfekt = String(Object.fromEntries(data.entries()).perfekt);
 	const prateritum = String(Object.fromEntries(data.entries()).prateritum);
 	ellenorzo("elkezdeni", nemet, perfekt, prateritum);
