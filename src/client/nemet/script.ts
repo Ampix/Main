13bd36fae440b5d0b55f82faed6fc76f609356bb
Blintx <balint.retvari@gmail.com>
2024-01-27 19:04:11 +0100

    test

--- src/client/nemet/script.ts
+++ src/client/nemet/script.ts
@@ -1,7 +1,8 @@
 import json from "./szavak.json";
 const szavak = json.szavak;
 // console.log(json);
-
+console.log(szavak[0].magyar === "elkezdeni");
+console.log("pityu");
 function szav_valaszt() {
 	const szó = document.getElementById("magyar");
 	if (szó) {
@@ -11,16 +12,26 @@ function szav_valaszt() {
 	}
 }
 szav_valaszt();
-function ellenorzo(magyar, nemet, perfekt, prateritum) {
-	if (szavak[szavak.indexOf(magyar) == magyar]) {
-		console.log("pryma");
-	}
+function ellenorzo(
+	magyar: string,
+	nemet: string,
+	perfekt: string,
+	prateritum: string,
+) {
+	console.log("siker");
+	const megoldasindex = szavak.find((element) => element.magyar === magyar);
+	console.log(megoldasindex);
+
+	// if (szavak[megoldasindex].magyar === magyar) {
+	// 	console.log("pryma");
+	// }
 }
 document.getElementById("nemet")?.addEventListener("submit", (e) => {
 	e.preventDefault();
 	const data = new FormData(e.target as HTMLFormElement);
-	const nemet = Object.fromEntries(data.entries()).nemet;
-	const perfekt = Object.fromEntries(data.entries()).perfekt;
-	const prateritum = Object.fromEntries(data.entries()).prateritum;
+	const nemet = String(Object.fromEntries(data.entries()).nemet);
+	const perfekt = String(Object.fromEntries(data.entries()).perfekt);
+	const prateritum = String(Object.fromEntries(data.entries()).prateritum);
+	ellenorzo("elkezdeni", nemet, perfekt, prateritum);
 	console.log(nemet, perfekt, prateritum);
 });
