3db17349056038134d8f17bc26e3baed99e5ccea
Blintx <balint.retvari@gmail.com>
2024-01-27 18:21:44 +0100

    form

--- src/client/nemet/script.ts
+++ src/client/nemet/script.ts
@@ -1,3 +1,21 @@
 import json from "./szavak.json";
-console.log("alma");
+const szavak = json;
 console.log(json);
+
+function szav_valaszt() {
+	const szó = document.getElementById("magyar");
+	if (szó) {
+		const index = 0;
+		console.log(szavak[index].magyar);
+		szó.innerText = szavak[index].magyar;
+	}
+}
+szav_valaszt();
+document.getElementById("nemet")?.addEventListener("submit", (e) => {
+	e.preventDefault();
+	const data = new FormData(e.target as HTMLFormElement);
+	const nemet = Object.fromEntries(data.entries()).nemet;
+	const perfekt = Object.fromEntries(data.entries()).perfekt;
+	const prateritum = Object.fromEntries(data.entries()).prateritum;
+	console.log(nemet, perfekt, prateritum);
+});
