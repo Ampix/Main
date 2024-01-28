43cefb7a310beb6f893ffb41f2601bf9d52618e1
HVCsano <hvcsano@proton.me>
2024-01-28 21:11:59 +0100

    mama

--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -6,13 +6,16 @@ import { api } from "../../main";
 export const router = express.Router();
 
 router.get("/", async (req, res) => {
+	const ip = req.ip ? req.ip : "nincs";
 	if (req.signedCookies.userAuthCode) {
 		const cucc = await fetch(
-			`${api}/auth/${req.signedCookies.userAuthCode}/${req.ip}`,
+			`${api}/users/auth/${req.signedCookies.userAuthCode}/${ip}`,
 		);
-		if (cucc) {
+		if (cucc.status === 200) {
 			res.sendFile(path.resolve("src/routes/user/index.html"));
 			console.log(cucc);
+		} else {
+			res.redirect(303, "/user/login");
 		}
 	} else {
 		res.redirect(303, "/user/login");
@@ -53,6 +56,7 @@ router.post("/post-login", async (req, res) => {
 
 router.post("/post-code", async (req, res) => {
 	const body = await req.body;
+	const ip = req.ip ? req.ip : "nincs";
 	if (body?.code) {
 		const cucc = await fetch(`${api}/users/login/code`, {
 			method: "POST",
@@ -65,9 +69,15 @@ router.post("/post-code", async (req, res) => {
 			}),
 		});
 		const mama = await cucc.text();
-		console.log(mama);
+		if (!mama) return res.redirect(303, "/user/login/code");
+		await fetch(`${api}/users/bind/addresstocode/${mama}/${ip}`);
+		res.cookie("userAuthCode", mama, {
+			signed: true,
+			secure: true,
+			sameSite: true,
+		});
 		if (cucc.status === 200) {
-			res.redirect(303, "/user/login/done");
+			res.redirect(303, "/user");
 		} else {
 			res.sendStatus(cucc.status);
 		}
