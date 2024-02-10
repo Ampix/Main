7afe4cc030dc24661f3703feb0a615ac720fb5d7
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-02-10 14:50:48 +0100

    noice

--- pages/index.vue
+++ pages/index.vue
@@ -3,10 +3,7 @@
 <template>
     <body class="bg-slate-700 text-white">
         <div class="flex h-screen">
-            <div class="m-auto text-center">
-                <h1 class="font-semibold text-3xl">Üdvözöllek az Ampixon!</h1>
-                <h2>Hamarosan lesz itt minden is!</h2>
-            </div>
+            <div class="m-auto text-center"></div>
         </div>
     </body>
 </template>
