2eb63563392c8b76e9b324c93ca275ea64a0d2a0
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-02-10 13:10:55 +0100

    alapok

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+    app: {
+        head: {
+            charset: 'utf-8',
+            viewport: 'width=device-width, initial-scale=1',
+            title: 'Ampix',
+        },
+    },
+    devtools: { enabled: true },
+    modules: ['@nuxtjs/tailwindcss'],
 })
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+    <body class="bg-slate-700 text-white">
+        <div class="flex h-screen">
+            <div class="m-auto text-center">
+                <h1 class="font-semibold text-3xl">Üdvözöllek az Ampixon!</h1>
+                <h2>Hamarosan lesz itt minden is!</h2>
+            </div>
+        </div>
+    </body>
+</template>
