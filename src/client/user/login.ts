35df77a54b018e4a8962acf3dc51fb187e872edb
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-02-10 11:46:36 +0100

    alapok

--- src/client/user/login.ts
+++
@@ -1,4 +0,0 @@
-document.getElementById("loginbtn")?.addEventListener("click", (ev) => {
-	ev.preventDefault();
-	window.open("/user/login/pop", "_blank", "height=400, width=550");
-});
--- src/routes/vakterkep/index.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import path from "node:path";
-
-export const router = Router();
-
-router.get("/", (req, res) => {
-	res.sendFile(path.resolve("src/routes/vakterkep/index.html"));
-});
