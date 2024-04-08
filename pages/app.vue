80523e374c1997f5d986404fcf1729a43d3a4f2d
HVCsano <55344602+HVCsano@users.noreply.github.com>
2024-04-08 16:56:57 +0200

    app

---
+++ pages/app.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1 class="text-white text-center font-bold">Átirányítás...</h1>
+    </div>
+</template>
+
+<script lang="ts" setup>
+await navigateTo(
+    'https://github.com/Ampix/App/releases/latest/download/ampix-app-setup.exe',
+    { external: true }
+)
+</script>
