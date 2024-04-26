8c86a10101b7993c5b38aab27fe9ecd402bd7792
Blintx40 <114827960+blintx@users.noreply.github.com>
2024-04-26 22:56:18 +0200

    newMatchButton

--- src/routes/sudoku/+layout.svelte
+++ src/routes/sudoku/+layout.svelte
@@ -2,6 +2,9 @@
 	import { onMount } from 'svelte';
 
 	let navigationBar: HTMLDivElement;
+	let NewMatch: HTMLDivElement;
+
+	
 	onMount(() => {
 		// navigationBar.onmouseenter = function hoverNavigationBar(e) {
 		// 	navigationBar.classList.remove('bg-blue-700');
@@ -13,9 +16,12 @@
 <div class="">
 	<div class="fixed bottom-0 left-0 flex h-10 w-screen justify-center bg-black">
 		<div
-			class="fixed bottom-5 flex h-20 w-1/3 rounded-xl bg-blue-700 transition-all duration-200 hover:bottom-6 hover:h-24 hover:w-1/2"
+			class=" items-center fixed grid grid-cols-5 bottom-5  h-20 w-1/3 rounded-xl bg-blue-700 transition-all duration-200 hover:bottom-6 hover:h-24 hover:w-1/2"
 			bind:this={navigationBar}
-		></div>
+		>
+	<div class="bg-slate-700 w-10 h-10 rounded-xl" bind:this={NewMatch}></div>
+	
+	</div>
 	</div>
 	<slot class=""></slot>
 </div>
