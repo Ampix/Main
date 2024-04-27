137889475fc644e1d0c3137f4da0d061cf859088
Blintx40 <114827960+blintx@users.noreply.github.com>
2024-04-27 13:04:05 +0200

    modal

--- src/routes/sudoku/+layout.svelte
+++ src/routes/sudoku/+layout.svelte
@@ -18,26 +18,26 @@
 	});
 </script>
 
-<!-- <Modal></Modal> -->
 <div class="">
 	<div class="fixed bottom-0 left-0 flex h-10 w-screen justify-center bg-black">
 		<div
 			class=" group fixed bottom-5 grid h-20 w-screen grid-cols-3 items-center rounded-xl bg-blue-700 transition-all duration-200 hover:bottom-6 hover:h-24"
 			bind:this={navigationBar}
-		>
+			>
 			<div
 				class="relative m-auto size-10 rounded-xl bg-slate-700 transition-all duration-200 group-hover:size-16"
 				bind:this={Settings}
-			></div>
-			<div
+				></div>
+				<div
 				class="relative m-auto size-14 rounded-2xl bg-slate-700 transition-all duration-200 group-hover:size-20"
 				bind:this={NewMatch}
-			></div>
-			<div
+				></div>
+				<div
 				class="relative m-auto size-10 rounded-xl bg-slate-700 transition-all duration-200 group-hover:size-16"
 				bind:this={Solver}
 			></div>
 		</div>
 	</div>
 	<slot></slot>
+	<Modal></Modal>
 </div>
--- src/routes/sudoku/DificultySelectermodal.svelte
+++ src/routes/sudoku/DificultySelectermodal.svelte
@@ -1,4 +1,17 @@
 <script lang="ts">
+	import { onMount } from "svelte";
+
+let modal: HTMLDivElement;
+let outerModal:HTMLDivElement;
+onMount(()=>{
+
+    
+})
 </script>
 
-<div class="h-screen w-screen bg-black" id="modal"></div>
+<div bind:this={outerModal} class="h-screen w-screen hidden items-center grid grid-cols-1 grid-rows-1 justify-center fixed bottom-0 left-0">
+	<div class="align trainsition-all duration-200 h-3/4 w-1/2 bg-gray-500 rounded-md grid grid-rows-7 overflow-auto m-auto" bind:this={modal}>
+    
+    
+    </div>
+</div>
