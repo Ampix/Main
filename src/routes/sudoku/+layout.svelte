1eb8e104b2665fed79afa0dec693133b46b304ba
Blintx40 <114827960+blintx@users.noreply.github.com>
2024-04-27 10:22:11 +0200

    button1

--- src/routes/sudoku/+layout.svelte
+++ src/routes/sudoku/+layout.svelte
@@ -16,12 +16,12 @@
 <div class="">
 	<div class="fixed bottom-0 left-0 flex h-10 w-screen justify-center bg-black">
 		<div
-			class=" items-center fixed grid grid-cols-5 bottom-5  h-20 w-1/3 rounded-xl bg-blue-700 transition-all duration-200 hover:bottom-6 hover:h-24 hover:w-1/2"
+			class=" group items-center fixed grid grid-cols-5 bottom-5  h-20 w-1/3 rounded-xl bg-blue-700 transition-all duration-200 hover:bottom-6 hover:h-24 hover:w-1/2"
 			bind:this={navigationBar}
 		>
-	<div class="bg-slate-700 w-10 h-10 rounded-xl" bind:this={NewMatch}></div>
+	<div class="bg-slate-700 w-10	 h-10 rounded-xl relative left-5 group-hover:size- size-1/2" bind:this={NewMatch}></div>
 	
 	</div>
 	</div>
-	<slot class=""></slot>
+	<slot></slot>
 </div>
