2ddf59ef38be9a7048e9fdc1de9f0c1f27f2aad2
Blintx40 <114827960+blintx@users.noreply.github.com>
2024-05-05 20:44:16 +0200

    nem jó

--- src/routes/sudoku/+page.svelte
+++ src/routes/sudoku/+page.svelte
@@ -1,4 +1,22 @@
 <script lang="ts">
+	import { getPossibleDigits } from './solver';
+	import { onMount } from 'svelte';
+	onMount(() => {
+		console.log(
+			getPossibleDigits([
+				{
+					value: 1,
+					pos: [0, 0],
+					section: [0, 0],
+					posibility: []
+				},
+				{
+					value: 0,
+					pos: [0, 1],
+					section: [0, 0],
+					posibility: []
+				}
+			])
+		);
+	});
 </script>
-
-kakcsi
--- src/routes/sudoku/solver.ts
+++ src/routes/sudoku/solver.ts
@@ -1,9 +1,47 @@
 import type { digitType, solverOutput } from './types.ts';
 
-function getPossibleDigits() {}
+export function getPossibleDigits(grid: digitType[]) {
+	const modifiedGrid = [];
+	for (let i = 0; i < grid.length; i++) {
+		const digit = grid[i];
+		digit.posibility = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+		if (digit.value !== 0) {
+			for (let j = 0; j < grid.length; j++) {
+				const djgjt = grid[i];
+				if (
+					digit.pos[0] === djgjt.pos[0] ||
+					digit.pos[1] === djgjt.pos[1] ||
+					digit.section === djgjt.section ||
+					djgjt.value !== 0
+				) {
+					digit.posibility.splice(digit.posibility.indexOf(djgjt.value) - 1, 1);
+				}
+			}
+		}
+		modifiedGrid.push(digit);
+	}
+	return modifiedGrid;
+}
 
 function solve(grid: digitType[]): solverOutput {
 	let output: solverOutput = { possibilities: 0, solved: grid };
 
 	return output;
 }
+
+console.log(
+	getPossibleDigits([
+		{
+			value: 1,
+			pos: [0, 0],
+			section: [0, 0],
+			posibility: []
+		},
+		{
+			value: 0,
+			pos: [0, 1],
+			section: [0, 0],
+			posibility: []
+		}
+	])
+);
